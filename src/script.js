c193666f8875bb394c93acc1248cc5d33c5ecd20
spargapees <kozhakov.azamat@gmail.com>
2023-03-20 22:11:38 +0600

    SprintTask3

--- src/script.js
+++ src/script.js
@@ -4,20 +4,15 @@ function go(){
     let index = x.selectedIndex;
     if(index == 0){
         document.getElementById('kzt').innerHTML = "-";
-        document.getElementById('usd').innerHTML = "-";
-        document.getElementById('eur').innerHTML = "-";
-    }
-    if(index == 1){
-        document.getElementById('kzt').innerHTML = "-";
         document.getElementById('usd').innerHTML = (sum.value / 431).toFixed(2);
         document.getElementById('eur').innerHTML = (sum.value / 509).toFixed(2);
     }
-    if(index == 2){
+    if(index == 1){
         document.getElementById('eur').innerHTML = "-";
         document.getElementById('kzt').innerHTML = (sum.value * 507).toFixed(2);
         document.getElementById('usd').innerHTML = (sum.value * 1.176).toFixed(2);
     }
-    if(index == 3){
+    if(index == 2){
         document.getElementById('usd').innerHTML = "-";
         document.getElementById('kzt').innerHTML = (sum.value * 429).toFixed(2);
         document.getElementById('eur').innerHTML = (sum.value * 0.843).toFixed(2);
